707f25605bf3cad5e03ef9b7f94f58eabd0ad52f
rakibul-romu <rakibulislamromu@gmail.com>
2025-08-28 21:58:37 +0600

    second commit

---
+++ assets/project.js
@@ -0,0 +1,11 @@
+const countElement = document.getElementById('count');
+let count = 0;
+
+const lifeIcons = document.querySelectorAll('life');
+
+for (let i = 0; i < lifeIcons.length; i++) {
+    lifeIcons[i].addEventListener('click', function() {
+        count++;
+        countElement.textContent = count;
+    });
+}
